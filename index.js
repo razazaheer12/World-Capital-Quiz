2d18edd4915a54506dc1e2870e72ef7e6ae753da
Raza Zaheer <razazaheer2002@gmail.com>
2026-06-04 02:50:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,28 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "World",
+  password: "starboy12",
+  port: 5432,
+});
 
-// Vercel Postgres connection: prefer DATABASE_URL if available
-const db = new pg.Client(
-  process.env.DATABASE_URL
-    ? { connectionString: process.env.DATABASE_URL }
-    : {
-        user: process.env.PGUSER,
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        password: process.env.PGPASSWORD,
-        port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
-      }
-);
+const app = express();
+const port = 3000;
+
+db.connect();
 
 let quiz = [];
-let quizLoaded = false;
-
-async function loadQuiz() {
-  if (quizLoaded) return;
-  await db.connect();
-  const res = await db.query("SELECT * FROM capitals");
-  quiz = res.rows;
-  quizLoaded = true;
-}
-
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
@@ -42,9 +37,9 @@ let currentQuestion = {};
 app.get("/", async (req, res) => {
   totalCorrect = 0;
   await nextQuestion();
+  console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
-
 
 // POST a new post
 app.post("/submit", (req, res) => {
@@ -65,8 +60,6 @@ app.post("/submit", (req, res) => {
 });
 
 async function nextQuestion() {
-  await loadQuiz();
-
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
   currentQuestion = randomCountry;
 }
@@ -74,4 +67,3 @@ async function nextQuestion() {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
